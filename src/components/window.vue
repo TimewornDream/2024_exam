3eb93c786e638c31d34ea5c7d23a917a5ee6d209
TimewornDream <2497505099@qq.com>
2024-09-13 22:45:38 +0800

    feat: 完成密码和确认密码组件

--- src/components/window.vue
+++ src/components/window.vue
@@ -3,11 +3,15 @@
     <div :class="windowClass">
         <img src="../assets/genshin.png" class="genshin" :disabled="true">
         <emailInput/>
+        <passwordInput  @updatePW="updatePWHandler"/>
+        <ensurePwInput :register="register" :password="password"/>
     </div>
 </template>
 
 <script setup>
     import emailInput from './emailInput.vue';
+    import passwordInput from './passwordInput.vue';
+    import ensurePwInput from './ensurePwInput.vue'
 </script>
 <script>
 
@@ -17,7 +21,14 @@ export default {
     
     data(){
         return {
-            windowClass: ["window", "fadeInDown"]
+            windowClass: ["window", "fadeInDown"],
+            register: true,
+            password: ""
+        }
+    },
+    methods:{
+        updatePWHandler(pw){
+            this.password = pw
         }
     }
 }

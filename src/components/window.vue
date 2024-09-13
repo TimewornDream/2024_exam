f9f3b862262bbe6519d484f7aff31d7d22b245df
TimewornDream <2497505099@qq.com>
2024-09-13 15:56:44 +0800

    feat: 完成窗口组件

---
+++ src/components/window.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="windowClass">123</div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            msg: "你好",
+            windowClass: ["window", "fadeInDown"]
+        }
+    }
+}
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+ // @ts-ignore
 import App from './App.vue'
+import './assets/main.css'
 
 createApp(App).mount('#app')
